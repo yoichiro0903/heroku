0a48d9652dfbfe05ce0432b302a535a99ac5fc36
Yoichiro Watanabe <yoichiro@WatanabeYoichiro-no-MacBook-Air.local>
2016-04-30 16:46:02 +0900

    modified port description.

--- callback.php
+++ callback.php
@@ -145,8 +145,8 @@ function api_post_request($path, $post) {
     curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_HTTPPROXYTUNNEL, 1);
-    curl_setopt($curl, CURLOPT_PROXY, getenv("http://fixie:RI6IOrzzcysgZDI@velodrome.usefixie.com:80"));
-    //curl_setopt($curl, CURLOPT_PROXYPORT, 80);
+    curl_setopt($curl, CURLOPT_PROXY, getenv("http://fixie:RI6IOrzzcysgZDI@velodrome.usefixie.com"));
+    curl_setopt($curl, CURLOPT_PROXYPORT, 80);
 
     $output = curl_exec($curl);
     error_log($output);
